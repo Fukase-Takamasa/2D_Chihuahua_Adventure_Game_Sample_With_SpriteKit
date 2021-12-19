51095218edd71cfadf6ed6d64ca70cded54ca47b
深瀬 貴将 <fukase@arsaga.jp>
2021-12-20 06:33:37 +0900

    Initial Commit

---
+++ Vamos-a-jugar-al-SpriteKitTests/Vamos_a_jugar_al_SpriteKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  Vamos_a_jugar_al_SpriteKitTests.swift
+//  Vamos-a-jugar-al-SpriteKitTests
+//
+//  Created by 深瀬 貴将 on 2021/12/20.
+//
+
+import XCTest
+@testable import Vamos_a_jugar_al_SpriteKit
+
+class Vamos_a_jugar_al_SpriteKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Vamos-a-jugar-al-SpriteKitUITests/Vamos_a_jugar_al_SpriteKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Vamos_a_jugar_al_SpriteKitUITestsLaunchTests.swift
+//  Vamos-a-jugar-al-SpriteKitUITests
+//
+//  Created by 深瀬 貴将 on 2021/12/20.
+//
+
+import XCTest
+
+class Vamos_a_jugar_al_SpriteKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
